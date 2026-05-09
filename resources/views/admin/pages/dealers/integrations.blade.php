490e0732773c56f4c6013cb2377cacac8879268f
RiyanTazim <tazimt8@gmail.com>
2026-05-09 06:06:29 +0600

    feat: add dealer integration management view for third-party services

--- resources/views/admin/pages/dealers/integrations.blade.php
+++ resources/views/admin/pages/dealers/integrations.blade.php
@@ -204,6 +204,7 @@
 
 @push('page-scripts')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     $(document).ready(function() {
         // Handle Form Submission
