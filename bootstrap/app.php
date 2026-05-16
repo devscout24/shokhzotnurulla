f28ea9d3d57050d6f7f79b69253ad7b45dd34654
Moskov-1 <wwwronyraihan123@gmail.com>
2026-05-17 03:36:59 +0600

    trusuted policies explicitend stated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: [
+            '127.0.0.1',
+        ]);
+
         $middleware->web(append: [
             TeamsPermission::class,
         ]);
