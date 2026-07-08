a09cfd57f8e99f00df36db4730a6be6288e4926f
unknown <wwwronyraihan123@gmail.com>
2026-07-08 14:24:09 +0600

    feat: implement ApplyPhotoOverlay job to dynamically add dealer branding and contact information to vehicle photos

--- app/Jobs/ApplyPhotoOverlay.php
+++ app/Jobs/ApplyPhotoOverlay.php
@@ -168,170 +168,105 @@ class ApplyPhotoOverlay implements ShouldQueue
 
         /*
         |--------------------------------------------------------------------------
+        | SCALING FACTOR (Based on standard 800px baseline width)
+        |--------------------------------------------------------------------------
+        */
+        $scale = $imgW / 800.0;
+        $headerH = (int)(self::HEADER_HEIGHT * $scale);
+        $contactH = (int)(self::CONTACT_HEIGHT * $scale);
+        $guaranteeH = (int)(self::GUARANTEE_HEIGHT * $scale);
+
+        /*
+        |--------------------------------------------------------------------------
         | TOP HEADER
         |--------------------------------------------------------------------------
         */
 
-
-
-        $leftWidth =
-            (int)($imgW * .65);
-
-
-
-
-        // Dark dealer section
-
-        $image->drawRectangle(function($rect) use ($leftWidth){
-
-            $rect->at(0,0);
-
-            $rect->size(
-                $leftWidth,
-                self::HEADER_HEIGHT
-            );
-
-            $rect->background(
-                'rgba(15,25,50,0.95)'
-            );
-
+        // 1. Draw full white header background first
+        $image->drawRectangle(function($rect) use ($imgW, $headerH) {
+            $rect->at(0, 0);
+            $rect->size($imgW, $headerH);
+            $rect->background('ffffff');
         });
 
+        // 2. Draw Top Left Blue Banner (pointing end shape)
+        $blueBannerW = (int)($imgW * 0.53);
+        $blueBannerH = (int)(63 * $scale);
+        $arrowTipWidth = (int)(35 * $scale);
 
-
-
-        // White logo section
-
-        $image->drawRectangle(function($rect) use ($imgW,$leftWidth){
-
-            $rect->at(
-                $leftWidth + 35,
-                0
-            );
-
-            $rect->size(
-                $imgW - $leftWidth,
-                self::HEADER_HEIGHT
-            );
-
-            $rect->background(
-                'ffffff'
-            );
-
+        $image->drawPolygon(function($polygon) use ($blueBannerW, $blueBannerH, $arrowTipWidth) {
+            $polygon->point(0, 0);
+            $polygon->point($blueBannerW, 0);
+            $polygon->point($blueBannerW + $arrowTipWidth, (int)($blueBannerH / 2));
+            $polygon->point($blueBannerW, $blueBannerH);
+            $polygon->point(0, $blueBannerH);
+            $polygon->background('3b698a');
         });
 
+        // 3. Draw Top Left Black Sub-Banner (slanted end shape)
+        $blackBannerH = $headerH - $blueBannerH;
 
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | CHEVRON
-        |--------------------------------------------------------------------------
-        */
-
-        $image->drawPolygon(function($polygon) use ($leftWidth) {
-            $polygon->point($leftWidth - 10, 0);
-            $polygon->point($leftWidth + 25, 0);
-            $polygon->point($leftWidth + 70, 47);
-            $polygon->point($leftWidth + 25, 95);
-            $polygon->point($leftWidth - 10, 95);
-            $polygon->point($leftWidth + 25, 47);
-            $polygon->background('ffffff');
+        $image->drawPolygon(function($polygon) use ($blueBannerW, $blueBannerH, $headerH, $scale) {
+            $polygon->point(0, $blueBannerH);
+            $polygon->point($blueBannerW - (int)(10 * $scale), $blueBannerH);
+            $polygon->point($blueBannerW - (int)(45 * $scale), $headerH);
+            $polygon->point(0, $headerH);
+            $polygon->background('000000');
         });
 
-
-
-
-        // Dealer name
-
+        // 4. Render Dealer Name (white text on blue banner)
+        $dealerNameSize = (int)(26 * $scale);
         $image->text(
             $dealerName,
-            25,
-            35,
-            function(FontFactory $font) use ($fontBold){
-
+            (int)(35 * $scale),
+            (int)(($blueBannerH / 2) + ($dealerNameSize * 0.35)),
+            function($font) use ($fontBold, $dealerNameSize) {
                 $font->file($fontBold);
-
-                $font->size(26);
-
+                $font->size($dealerNameSize);
                 $font->color('ffffff');
-
                 $font->align('left');
-
             }
         );
 
-
-
-
-        // Spanish text
-
+        // 5. Render Spanish Sub-Text (white text on black banner)
+        $subBannerSize = (int)(11 * $scale);
         $image->text(
             'HABLAMOS ESPAÑOL',
-            25,
-            70,
-            function(FontFactory $font) use ($fontBold){
-
+            (int)(35 * $scale),
+            $blueBannerH + (int)(($blackBannerH / 2) + ($subBannerSize * 0.35)),
+            function($font) use ($fontBold, $subBannerSize) {
                 $font->file($fontBold);
-
-                $font->size(14);
-
+                $font->size($subBannerSize);
                 $font->color('ffffff');
-
                 $font->align('left');
-
             }
         );
-                /*
-        |--------------------------------------------------------------------------
-        | LOGO
-        |--------------------------------------------------------------------------
-        */
 
-
+        // 6. Insert Dealer Logo to Top Right (if exists, scaled to fit)
         if (file_exists($logoPath)) {
+            $logoManager = new ImageManager(new Driver());
+            $logo = $logoManager->decode($logoPath);
 
+            $maxWidth = (int)(220 * $scale);
+            $maxHeight = $headerH - (int)(20 * $scale);
 
-            $logoManager =
-                new ImageManager(
-                    new Driver()
-                );
-
-
-            $logo =
-                $logoManager->decode($logoPath);
-
-
-
-            $maxWidth = 220;
-
-
-
-            $scale =
-                min(
-                    $maxWidth / $logo->width(),
-                    (self::HEADER_HEIGHT - 20) / $logo->height()
-                );
-
-
-
-            $logo->resize(
-                (int)($logo->width() * $scale),
-                (int)($logo->height() * $scale)
+            $scaleLogo = min(
+                $maxWidth / $logo->width(),
+                $maxHeight / $logo->height()
             );
 
-
+            $logo->resize(
+                (int)($logo->width() * $scaleLogo),
+                (int)($logo->height() * $scaleLogo)
+            );
 
             $image->insert(
                 $logo,
-                $imgW - $logo->width() - 20,
-                (int)((self::HEADER_HEIGHT - $logo->height()) / 2)
+                $imgW - $logo->width() - (int)(20 * $scale),
+                (int)(($headerH - $logo->height()) / 2),
+                'top-left'
             );
-
         }
-
-
-
 
         /*
         |--------------------------------------------------------------------------
@@ -339,210 +274,122 @@ class ApplyPhotoOverlay implements ShouldQueue
         |--------------------------------------------------------------------------
         */
 
+        $contactY = $imgH - $guaranteeH - $contactH;
 
-        $contactY =
-            $imgH -
-            self::GUARANTEE_HEIGHT -
-            self::CONTACT_HEIGHT;
-
-
-
-
-        $image->drawRectangle(function($rect) use ($imgW,$contactY){
-
-            $rect->at(
-                0,
-                $contactY
-            );
-
-
-            $rect->size(
-                $imgW,
-                self::CONTACT_HEIGHT
-            );
-
-
-            $rect->background(
-                'ffffff'
-            );
-
+        // 1. Draw black background for the bottom bar first
+        $image->drawRectangle(function($rect) use ($imgW, $contactY, $contactH) {
+            $rect->at(0, $contactY);
+            $rect->size($imgW, $contactH);
+            $rect->background('000000');
         });
 
+        // 2. Draw white polygon on the left (slanting down-right)
+        $bottomLeftW = (int)($imgW * 0.48);
+        $slant = (int)(36 * $scale);
 
-
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | CALL / TEXT BLOCK
-        |--------------------------------------------------------------------------
-        */
-
-
-        $image->drawRectangle(function($rect) use ($contactY){
-
-            $rect->at(
-                0,
-                $contactY
-            );
-
-
-            $rect->size(
-                90,
-                self::CONTACT_HEIGHT
-            );
-
-
-            $rect->background(
-                'rgba(15,25,50,0.95)'
-            );
-
+        $image->drawPolygon(function($polygon) use ($bottomLeftW, $contactY, $contactH, $slant) {
+            $polygon->point(0, $contactY);
+            $polygon->point($bottomLeftW, $contactY);
+            $polygon->point($bottomLeftW - $slant, $contactY + $contactH);
+            $polygon->point(0, $contactY + $contactH);
+            $polygon->background('ffffff');
         });
 
+        // 3. Render CALL/TEXT stacked label in white section
+        $lblSize = (int)(11 * $scale);
+        $image->text('CALL', (int)(45 * $scale), $contactY + (int)(21 * $scale), function($font) use ($fontBold, $lblSize) {
+            $font->file($fontBold);
+            $font->size($lblSize);
+            $font->color('0f1932');
+            $font->align('center');
+        });
+        $image->text('TEXT', (int)(45 * $scale), $contactY + (int)(36 * $scale), function($font) use ($fontBold, $lblSize) {
+            $font->file($fontBold);
+            $font->size($lblSize);
+            $font->color('0f1932');
+            $font->align('center');
+        });
 
-
-
-        $image->text(
-            "CALL\nTEXT",
-            45,
-            $contactY + 28,
-            function(FontFactory $font) use ($fontBold){
-
-
-                $font->file($fontBold);
-
-                $font->size(11);
-
-                $font->color('ffffff');
-
-                $font->align('center');
-
-
-            }
-        );
-
-
-
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | PHONE
-        |--------------------------------------------------------------------------
-        */
-
-
+        // 4. Render Phone Number in white section
+        $phoneSize = (int)(30 * $scale);
         $image->text(
             $dealerPhone,
-            (int)($imgW * .35),
-            $contactY + 35,
-            function(FontFactory $font) use ($fontBold){
-
-
+            (int)(90 * $scale),
+            $contactY + (int)(($contactH / 2) + ($phoneSize * 0.32)),
+            function($font) use ($fontBold, $phoneSize) {
                 $font->file($fontBold);
-
-                $font->size(30);
-
-                $font->color('0f1932');
-
-                $font->align('center');
-
-
+                $font->size($phoneSize);
+                $font->color('3b698a');
+                $font->align('left');
             }
         );
 
+        // 5. Render Domain in black section
+        if ($displayDomain) {
+            $displayDomain = strtoupper($displayDomain);
+            $domainSize = (int)(18 * $scale);
+            $rightX = $imgW - (int)(30 * $scale);
+            $centerY = $contactY + (int)(($contactH / 2) + ($domainSize * 0.32));
 
+            if (str_starts_with($displayDomain, 'WWW.')) {
+                $prefix = 'WWW.';
+                $mainDomain = substr($displayDomain, 4);
 
+                // Right-align the main domain in white
+                $image->text($mainDomain, $rightX, $centerY, function($font) use ($fontBold, $domainSize) {
+                    $font->file($fontBold);
+                    $font->size($domainSize);
+                    $font->color('ffffff');
+                    $font->align('right');
+                });
 
+                // Calculate width of main domain to place WWW. in blue (chars width approx 0.65 of size)
+                $charWidth = $domainSize * 0.60;
+                $mainDomainLength = strlen($mainDomain);
+                $prefixX = $rightX - (int)($mainDomainLength * $charWidth);
 
-
-        /*
-        |--------------------------------------------------------------------------
-        | DOMAIN
-        |--------------------------------------------------------------------------
-        */
-
-
-        $image->text(
-            strtoupper($displayDomain),
-            $imgW - 160,
-            $contactY + 35,
-            function(FontFactory $font) use ($fontBold){
-
-
-                $font->file($fontBold);
-
-                $font->size(17);
-
-                $font->color('0f1932');
-
-                $font->align('center');
-
-
+                $image->text($prefix, $prefixX, $centerY, function($font) use ($fontBold, $domainSize) {
+                    $font->file($fontBold);
+                    $font->size($domainSize);
+                    $font->color('3b698a');
+                    $font->align('right');
+                });
+            } else {
+                $image->text($displayDomain, $rightX, $centerY, function($font) use ($fontBold, $domainSize) {
+                    $font->file($fontBold);
+                    $font->size($domainSize);
+                    $font->color('ffffff');
+                    $font->align('right');
+                });
             }
-        );
-
-
-
-
+        }
 
         /*
         |--------------------------------------------------------------------------
-        | GUARANTEE BAR
+        | GUARANTEE BAR (FOOTER)
         |--------------------------------------------------------------------------
         */
 
+        $guaranteeY = $imgH - $guaranteeH;
 
-        $guaranteeY =
-            $imgH -
-            self::GUARANTEE_HEIGHT;
-
-
-
-
-
-        $image->drawRectangle(function($rect) use ($imgW,$guaranteeY){
-
-
-            $rect->at(
-                0,
-                $guaranteeY
-            );
-
-
-            $rect->size(
-                $imgW,
-                self::GUARANTEE_HEIGHT
-            );
-
-
-            $rect->background(
-                'rgba(15,25,50,0.95)'
-            );
-
-
+        // 1. Draw blue background for the footer
+        $image->drawRectangle(function($rect) use ($imgW, $guaranteeY, $guaranteeH) {
+            $rect->at(0, $guaranteeY);
+            $rect->size($imgW, $guaranteeH);
+            $rect->background('3b698a');
         });
 
-
-
-
-
+        // 2. Render footer text
+        $footerSize = (int)(14 * $scale);
         $image->text(
             'GUARANTEED APPROVAL & 3-MONTH/3000-MILE WARRANTY',
-            $imgW / 2,
-            $guaranteeY + 20,
-            function(FontFactory $font) use ($fontBold){
-
-
+            (int)($imgW / 2),
+            $guaranteeY + (int)(($guaranteeH / 2) + ($footerSize * 0.32)),
+            function($font) use ($fontBold, $footerSize) {
                 $font->file($fontBold);
-
-                $font->size(15);
-
+                $font->size($footerSize);
                 $font->color('ffffff');
-
                 $font->align('center');
-
-
             }
         );
                 /*
@@ -552,7 +399,7 @@ class ApplyPhotoOverlay implements ShouldQueue
         */
 
 
-        $image->save($outputPath);
+        $image->save($outputPath, quality: 95);
 
 
 
